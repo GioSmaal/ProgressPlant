a01ce54c3c397ae5e3dcae9bdb9c09b381ffd024
i432855 <432855@student.fontys.nl>
2021-10-06 13:12:30 +0200

    Animation stem basic

--- ProgressPlant.js
+++ ProgressPlant.js
@@ -8,15 +8,16 @@ function button1() {
 
     newStem.id = "id" + countNew;
 
-    newStem.appendChild(constantButton);
+    // newStem.appendChild(constantButton);
 
     plant.appendChild(newStem);
 
-    document.getElementById("id" + countNew).style.cssText = "width: 45px;height: 300px;background-color: #7FED0C;position: absolute;border-radius: 64% 36% 52% 48% / 61% 24% 76% 39%;left: -30px;"
-    ;
+    document.getElementById("id" + countNew).className = "stem2";
     document.getElementById("id" + countNew).style.top = count + "px";
-    count = count + 160;
-    console.log(count);
-
+    count = count + 190;
     countNew++;
+
+    newStem.addEventListener("animationend", function() {
+        newStem.appendChild(constantButton);
+    })
 }
